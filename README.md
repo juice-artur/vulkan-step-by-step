0f8dc90582c1f1d44a41677485eaa36e5b95c508
juice-artur <asasin.lol10@gmail.com>
2026-04-20 16:06:11 +0300

    Cleanup

--- README.md
+++
@@ -1,8 +0,0 @@
-# vulkan-step-by-step
-
----
-# Project setup
-- Cloning
-    ```bash
-    git clone https://github.com/juice-artur/vulkan-step-by-step.git --recursive
-    ```
--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "VulkanEngine.h"
-int main(){
-    VulkanEngine vulkanEngine{};
-    vulkanEngine.init();
-    vulkanEngine.run();
-    vulkanEngine.cleanup();
-    return 0;
-};
